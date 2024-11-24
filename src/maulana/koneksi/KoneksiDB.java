28a8722980b29237b310ffa587f566c15f238616
maulanasaldi <mmaulanasaldi@gmail.com>
2024-11-24 22:30:33 +0700

    Update again and again

--- src/maulana/koneksi/KoneksiDB.java
+++ src/maulana/koneksi/KoneksiDB.java
@@ -1,29 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package maulana.koneksi;
 import java.sql.*;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author saldi
- */
 public class KoneksiDB {
     private Connection koneksi;
-    public Connection connect(){
+    public Connection connect(){       
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Koneksi berhasil");
         } catch (ClassNotFoundException ex) {
-            System.out.println("Gagal koneksi"+ex);
+            System.out.println("Gagal koneksi"+ex);           
         }
         String url = "jdbc:mysql://localhost:3306/car-rental";
         try {
             koneksi = DriverManager.getConnection(url,"root","");
             System.out.println("Berhasil koneksi database");
         } catch (SQLException ex) {
-            System.out.println("Gagal koneksi database"+ex);
+            JOptionPane.showMessageDialog(null, 
+            "MySQL belum di aktifkan.\nSilakan aktifkan MySQL terlebih dahulu untuk terhubung dengan database.", 
+            "Koneksi Gagal", 
+            JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+            System.out.println("Gagal koneksi database"+ex);            
         }
         return koneksi;
     }
